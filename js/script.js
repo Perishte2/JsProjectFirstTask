d7e7906c72d128d9a2a5e3f0306aea0775212c7f
Perishte <perishte010203@gmail.com>
2022-01-23 01:19:03 +0600

    added some functionalities with cicles and conditions

--- js/script.js
+++ js/script.js
@@ -1,32 +1,47 @@
 //first task
-let money = +prompt("Ваш бюджет на месяц");
+let money = prompt("Ваш бюджет на месяц", '');
 let time = prompt("Введите дату в формате YYYY-MM-DD");
 
 
-
-//second task
-let firstAnswer = prompt("Введите обязательную статью расходов в этом месяце");
-let secondAnswer = +prompt("Во сколько обойдется?");
-
-let expenses={
-    firstAnswer:secondAnswer
+let appData = {
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    timeData: time,
+    savings: false
 };
 
-console.log(expenses);
 
 
+for (let i = 0; i < 2; i++) {
+    let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt ("Во сколько обойдется?", "");
 
-//third task
- let appData = {budjet:money, timeData:time,
-expenses:secondAnswer,
-optionalExenses:"",
-income:"",
-savings:false
+    if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+        console.log ("done");
+
+        appData.expenses[a] = b;
+    } else {                            
+        console.log ("bad result");
+        i--;
+    }
+
 };
 
-console.log(appData);
 
+appData.moneyPerDay = appData.budget / 30;
+alert("Ежедневный бюджет : " + appData.moneyPerDay);
 
+if(appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if(appData.moneyPerDay > 100 && appData.moneyPerDay < 2000 ){
+    console.log("Средний уровень достатка");
+} else if(appData.moneyPerDay > 2000 ) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Что то пошло не так");
+}
 
-alert(money);
 
